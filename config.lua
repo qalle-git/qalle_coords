ee2bb6d0a79432e0a17e8cd5768e21fb9448aa60
Puntherline <53500972+Puntherline@users.noreply.github.com>
2021-10-14 19:40:17 +0200

    Created config.lua
    - Created hopefully easy to understand `config.lua` file.

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+
+
+-- Basic text config
+Config.Font = 4                 -- Sets font            | 0 = Sans Serif | 1 = Script | 4 = Bold Sans Serif | 7 = Pricedown
+Config.Scale = 0.6              -- Sets scale           | 0.0 to 1.0
+Config.Decimals = 3             -- Number of decimals   | 0 to infinite
+Config.PositionX = 0.165        -- Horizontal position  | 0.0 to 1.0
+Config.PositionY = 0.86         -- Vertical position    | 0.0 to 1.0
+
+Config.ColourR = 255            -- Red colour channel   | 0 to 255
+Config.ColourG = 255            -- Green colour channel | 0 to 255
+Config.ColourB = 255            -- Blue colour channel  | 0 to 255
+Config.ColourA = 255            -- Alpha colour channel | 0 to 255
+
+
+
+-- Advanced text config
+Config.CustomSpacing = true     -- Enable custom space between lines, slight performance hit | "true" or "false"
+Config.Spacing = 0.03           -- Space between each line | 0.0 to 1.0
+
+
+
+-- Just for fun setting
+Config.RGB = false              -- Enable RGB text, slight performance hit | "true" or "false"
+Config.RGBSpeed = 0.3           -- Speed at which the text changes | 0 to infinite
